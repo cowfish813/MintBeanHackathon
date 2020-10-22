78a63a59968a553d0043ecb67c6a61b75a7f7a39
Nick_Cheung <nkkcheung@gmail.com>
2020-10-21 18:30:21 -0700

    eric code

--- public/script.js
+++ public/script.js
@@ -2,10 +2,16 @@ let menu = document.getElementById('help')
 let readme = document.getElementById('readme')
 
 
-menu.onclick = function() {
-    if (readme.style.display === "none") {
-        readme.style.display = "block";
-      } else {
-        readme.style.display = "none";
-      }
-}
+document.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+    readme.style.display = "none"
+  }
+})
+
+menu.onclick = function () {
+  if (readme.style.display === "none" || readme.style.display === "") {
+    readme.style.display = "block"
+  } else {
+    readme.style.display = 'none'
+  };
+};
